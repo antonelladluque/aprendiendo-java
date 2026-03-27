61ca57826d0448db146ac90bdf2c8f7cdd7c78ad
Anto Luque <105891198+antonelladluque@users.noreply.github.com>
2026-03-27 13:08:09 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int contador= 0;
+
+        do{
+           contador = contador +1;
+           System.out.println("vuelta" + contador);
+
+        }while (contador < 10);
+    }
+}
